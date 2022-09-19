747ef3baa67dfe2bd4da36957fcad59633f41619
Tuna Alaygut <alaygut@gmail.com>
2022-09-19 22:53:21 +0300

    ISSUE #19: Image viewer
    Minor refactors can be done as needed.

---
+++ src/data_prep/rimg_viewer.py
@@ -0,0 +1,29 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+INPUT_RIMG_PATH = sys.argv[1]
+
+
+def main():
+    pixels = ""
+    with open(INPUT_RIMG_PATH, "r+") as f:
+        pixels = f.read().split("$")[0]
+
+    plt.imshow(__str_to_ndarray(pixels.strip()))
+    plt.show()
+
+
+def __str_to_ndarray(str: str) -> np.ndarray:
+    rows = str.split('\n')
+    arr = np.ndarray((len(rows), len(rows[0].split())))
+    
+    for idx, row in enumerate(rows):
+        arr[idx, :] = row.split()
+    
+    return arr
+
+
+if __name__ == "__main__":
+    main()
