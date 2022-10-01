d75481a9dd0a1c682fb633c9ecf75b120969d471
Tuna Alaygut <alaygut@gmail.com>
2022-10-01 23:02:07 +0300

    ISSUE Find a way to call training XOR verification #66 Arg parser is added which allows us to give a verification flag. This commit also fixes Implement `argparser` to parse various inputs to programs #42

--- src/dqn/main.py
+++ src/dqn/main.py
@@ -1,32 +1,40 @@
-import os
-import sys
-
 from verification import test_market_neutralized_portfolio
-sys.path.append("..")
-from rlogging.training_logger import TrainingLogger
-from utility.util import read_config
 from data_reader import DataReader
 from train import Agent
+import argparse
+from rlogging.training_logger import TrainingLogger
+from utility.util import read_config
+import os
 
-# Command line arguments
-# TODO: Implement arg parser instead of this
-DATA_DIR = sys.argv[1]
-CONFIG_FILENAME = sys.argv[2]
-MODEL_PATH = ""
 
-try:
-    MODEL_PATH = sys.argv[3]
-except IndexError as e:
-    pass
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input-path', required=True, type=str,
+                    help="Path to input data.")
+parser.add_argument('-c', '--config', required=True, type=str,
+                    help="Name of the config file.")
+parser.add_argument('-v', '--verification', action="store_true",
+                    help="Whether to work in training or verification mode.")
+parser.add_argument("-m", "--model", required=False, type=str,
+                    help="Path to pretrained model. Required if -v is set.")
+args = vars(parser.parse_args())
 
 # Globals
+DATA_DIR = args["input_path"]
+CONFIG_FILENAME = args["config"]
+IS_TRAINING = not args["verification"]
+MODEL_PATH = args["model"] if args["verification"] else None
+if args["verification"] and args["model"] is None:
+    parser.error("-v requires -m")
+
 TICKERS = os.listdir(DATA_DIR)
 DATA_DIRS = [os.path.join(DATA_DIR, curr_data) for curr_data in TICKERS]
 OUTPUT_DIR = "output"
 
 
 def main():
-    config = read_config(config_filename=CONFIG_FILENAME, output_dir=OUTPUT_DIR)
+    config = read_config(config_filename=CONFIG_FILENAME,
+                         output_dir=OUTPUT_DIR)
 
     agent = Agent(config)
     data_reader = DataReader()
@@ -34,9 +42,9 @@ def main():
     X, y = data_reader.read(DATA_DIRS)
     agent.set_data(X, y, config)
 
-    logger = TrainingLogger(config=config, tickers=TICKERS, 
-                            output_dir=OUTPUT_DIR)
-    if MODEL_PATH == "":
+    if IS_TRAINING:
+        logger = TrainingLogger(config=config, tickers=TICKERS,
+                                output_dir=OUTPUT_DIR)
         agent.train(config, logger=logger)
         logger.save()
     else:
