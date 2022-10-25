e11ae221e0d274823f42471d1e799d59c90332c3
Tuna Alaygut <alaygut@gmail.com>
2022-10-25 22:48:27 +0300

    Issue Find a way to parameterize verification date range #139 Start and end indices are added

--- src/dqn/data_reader.py
+++ src/dqn/data_reader.py
@@ -24,13 +24,13 @@ class DataReader:
         print('numof all data: ', len(pathlist))
         return pathlist
 
-    def read(self, data_dirs: list, limit=None) -> tuple:
+    def read(self, data_dirs: list, start_idx=0, end_idx=None) -> tuple:
         X, y = [], []
         
         date_begin, date_end = None, None
-        first_file = natsorted(os.listdir(data_dirs[0]))[0]
-        last_file = natsorted(
-            os.listdir(data_dirs[0]))[limit -1 if limit else -1]
+        first_file = natsorted(os.listdir(data_dirs[0]))[start_idx]
+        last_file = natsorted(os.listdir(data_dirs[0]))[
+            end_idx if end_idx else -1]
 
         with open(os.path.join(data_dirs[0], first_file), "r+") as f:
             date_begin = f.read().split("$")[-1].strip()
@@ -39,13 +39,12 @@ class DataReader:
 
         for data_dir in natsorted(data_dirs):
             X_sub, y_sub = [], []
-            for rimg_file in natsorted(os.listdir(data_dir)):
+            for rimg_file in natsorted(os.listdir(data_dir))[start_idx:end_idx]:
                 filepath = os.path.join(data_dir, rimg_file)
                 with open(filepath, "r+") as f:
                     X_part, y_part, _ = f.read().split("$")
                     X_sub.append(str_to_ndarray(X_part.strip()))
                     y_sub.append(float(y_part.strip()))
-                if(limit and len(X_sub) == limit): break
             X.append(X_sub)
             y.append(y_sub)
 
