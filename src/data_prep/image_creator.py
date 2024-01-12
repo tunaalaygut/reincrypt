c1d49d59596ba00f6a20ddd489aa91414140177d
Tuna Alaygut <alaygut@gmail.com>
2024-01-12 14:04:09 +0300

    Added the ability to train and verify 1D network

--- src/data_prep/image_creator.py
+++ src/data_prep/image_creator.py
@@ -1,7 +1,6 @@
 import os
 import sys
 sys.path.append("..")
-import talib as ta
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import minmax_scale
@@ -17,6 +16,8 @@ parser.add_argument('-o', '--outdir', required=True, type=str,
                     help="Output (rimg) data directory.")
 parser.add_argument('-t', '--type', required=True, type=str, 
                     help="Image creation type <train> or <verification>")
+parser.add_argument("-1d", "--one-d", action="store_true",
+                    help="Whether to create a 1D rimg")
 args = vars(parser.parse_args())
 
 INPUT_DIR = args["indir"]
@@ -26,6 +27,7 @@ IS_TRAIN = (TYPE == "train")
 IMAGE_SIZE = 32
 INTERVAL_START = 7
 TICKERS = []
+ONE_D = args["one_d"]
 
 if IS_TRAIN:
     with open("../../training_currencies.txt", "r+") as f: 
@@ -43,73 +45,86 @@ def main():
                 continue
             df = pd.read_csv(os.path.join(INPUT_DIR, input_file))
             df = (clean_anomalies(df) if IS_TRAIN else df)
+
+            images = None
             
             print(f"\nTrying to create image data for {currency_symbol}.")
-            tis = np.zeros((IMAGE_SIZE, IMAGE_SIZE, len(df)))
-            intervals = range(INTERVAL_START, INTERVAL_START + IMAGE_SIZE)
-            
-            for interval in intervals:
-                i = interval - intervals.start
-                # Cluster 0 based on technical indicator clustering
-                tis[i][0] = ta.EMA(real=df.Close, timeperiod=interval)  
-                tis[i][1] = ta.KAMA(real=df.Close, timeperiod=interval)  
-                tis[i][2] = ta.MIDPOINT(real=df.Close, timeperiod=interval)  
-                tis[i][3] = ta.SMA(real=df.Close, timeperiod=interval)  
-                tis[i][4] = ta.T3(real=df.Close, timeperiod=interval, 
-                                  vfactor=0.7)  
-                tis[i][5] = ta.TRIMA(real=df.Close, timeperiod=interval)  
-                tis[i][6] = ta.WMA(real=df.Close, timeperiod=interval)  
-                tis[i][7] = ta.MA(real=df.Close, timeperiod=interval)  
-                tis[i][8] = ta.MIDPRICE(high=df.High, low=df.Low, 
-                                        timeperiod=interval)  
-                # Cluster 1 based on technical indicator clustering
-                tis[i][9] = ta.DEMA(real=df.Close, timeperiod=interval)  
-                tis[i][10] = ta.CMO(real=df.Close, timeperiod=interval)
-                tis[i][11] = ta.RSI(real=df.Close, timeperiod=interval)
-                tis[i][12] = ta.ULTOSC(high=df.High, low=df.Low, close=df.Close, 
-                                       timeperiod1=interval, 
-                                       timeperiod2=interval*2,
-                                       timeperiod3=interval*4)
-                tis[i][13] = ta.PLUS_DI(high=df.High, low=df.Low, 
-                                        close=df.Close, timeperiod=interval)
-                tis[i][14] = ta.ADOSC(high=df.High, low=df.Low, close=df.Close, 
-                                      volume=df.Volume, fastperiod=interval-4,
-                                      slowperiod=interval+3) 
-                # Cluster 2 based on technical indicator clustering
-                tis[i][15] = ta.ADX(high=df.High, low=df.Low, close=df.Close, 
-                                    timeperiod=interval)
-                tis[i][16] = ta.CCI(high=df.High, low=df.Low, close=df.Close, 
-                                    timeperiod=interval)
-                tis[i][17] = ta.DX(high=df.High, low=df.Low, close=df.Close, 
-                                   timeperiod=interval)
-                tis[i][18] = ta.MOM(real=df.Close, timeperiod=interval)
-                tis[i][19] = ta.MFI(high=df.High, low=df.Low, close=df.Close, 
-                                    volume=df.Volume, timeperiod=interval)
-                tis[i][20] = ta.WILLR(high=df.High, low=df.Low, close=df.Close, 
-                                      timeperiod=interval)
-                tis[i][21] = ta.ADXR(high=df.High, low=df.Low, close=df.Close, 
-                                     timeperiod=interval)
-                tis[i][22] = ta.AROONOSC(high=df.High, low=df.Low,
-                                         timeperiod=interval)
-                tis[i][23] = ta.ROC(real=df.Close, timeperiod=interval)
-                tis[i][24] = ta.ROCP(real=df.Close, timeperiod=interval)
-                tis[i][25] = ta.TSF(real=df.Close, timeperiod=interval)
-                tis[i][26] = ta.LINEARREG(real=df.Close, timeperiod=interval)
-                # Cluster 3 based on technical indicator clustering
-                tis[i][27] = ta.PLUS_DM(high=df.High, low=df.Low,
-                                        timeperiod=interval)
-                tis[i][28] = ta.ATR(high=df.High, low=df.Low, close=df.Close, 
-                                    timeperiod=interval)
-                tis[i][29] = ta.NATR(high=df.High, low=df.Low, close=df.Close, 
-                                     timeperiod=interval)
-                tis[i][30] = ta.STDDEV(real=df.Close, timeperiod=interval)
-                tis[i][31] = ta.VAR(real=df.Close, timeperiod=interval)
+            if ONE_D:
+                seq_len = IMAGE_SIZE
+                tis = np.zeros((len(df), IMAGE_SIZE))
+                for i in range(seq_len, len(df) - seq_len):
+                    tis[i] = df.Close[i-seq_len:i].values
+                images = np.zeros((len(df), IMAGE_SIZE))
+                for day_idx in range(len(df)):
+                    images[day_idx] = tis[day_idx]
 
-            images = np.zeros((len(df), IMAGE_SIZE, IMAGE_SIZE))
-
-            for day_idx in range(len(df)):
-                images[day_idx] = tis[:, :, day_idx]
+            else:
+                import talib as ta
+                tis = np.zeros((IMAGE_SIZE, IMAGE_SIZE, len(df)))
+                intervals = range(INTERVAL_START, INTERVAL_START + IMAGE_SIZE)
                 
+                for interval in intervals:
+                    i = interval - intervals.start
+                    # Cluster 0 based on technical indicator clustering
+                    tis[i][0] = ta.EMA(real=df.Close, timeperiod=interval)  
+                    tis[i][1] = ta.KAMA(real=df.Close, timeperiod=interval)  
+                    tis[i][2] = ta.MIDPOINT(real=df.Close, timeperiod=interval)  
+                    tis[i][3] = ta.SMA(real=df.Close, timeperiod=interval)  
+                    tis[i][4] = ta.T3(real=df.Close, timeperiod=interval, 
+                                    vfactor=0.7)  
+                    tis[i][5] = ta.TRIMA(real=df.Close, timeperiod=interval)  
+                    tis[i][6] = ta.WMA(real=df.Close, timeperiod=interval)  
+                    tis[i][7] = ta.MA(real=df.Close, timeperiod=interval)  
+                    tis[i][8] = ta.MIDPRICE(high=df.High, low=df.Low, 
+                                            timeperiod=interval)  
+                    # Cluster 1 based on technical indicator clustering
+                    tis[i][9] = ta.DEMA(real=df.Close, timeperiod=interval)  
+                    tis[i][10] = ta.CMO(real=df.Close, timeperiod=interval)
+                    tis[i][11] = ta.RSI(real=df.Close, timeperiod=interval)
+                    tis[i][12] = ta.ULTOSC(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod1=interval, 
+                                        timeperiod2=interval*2,
+                                        timeperiod3=interval*4)
+                    tis[i][13] = ta.PLUS_DI(high=df.High, low=df.Low, 
+                                            close=df.Close, timeperiod=interval)
+                    tis[i][14] = ta.ADOSC(high=df.High, low=df.Low, close=df.Close, 
+                                        volume=df.Volume, fastperiod=interval-4,
+                                        slowperiod=interval+3) 
+                    # Cluster 2 based on technical indicator clustering
+                    tis[i][15] = ta.ADX(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod=interval)
+                    tis[i][16] = ta.CCI(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod=interval)
+                    tis[i][17] = ta.DX(high=df.High, low=df.Low, close=df.Close, 
+                                    timeperiod=interval)
+                    tis[i][18] = ta.MOM(real=df.Close, timeperiod=interval)
+                    tis[i][19] = ta.MFI(high=df.High, low=df.Low, close=df.Close, 
+                                        volume=df.Volume, timeperiod=interval)
+                    tis[i][20] = ta.WILLR(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod=interval)
+                    tis[i][21] = ta.ADXR(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod=interval)
+                    tis[i][22] = ta.AROONOSC(high=df.High, low=df.Low,
+                                            timeperiod=interval)
+                    tis[i][23] = ta.ROC(real=df.Close, timeperiod=interval)
+                    tis[i][24] = ta.ROCP(real=df.Close, timeperiod=interval)
+                    tis[i][25] = ta.TSF(real=df.Close, timeperiod=interval)
+                    tis[i][26] = ta.LINEARREG(real=df.Close, timeperiod=interval)
+                    # Cluster 3 based on technical indicator clustering
+                    tis[i][27] = ta.PLUS_DM(high=df.High, low=df.Low,
+                                            timeperiod=interval)
+                    tis[i][28] = ta.ATR(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod=interval)
+                    tis[i][29] = ta.NATR(high=df.High, low=df.Low, close=df.Close, 
+                                        timeperiod=interval)
+                    tis[i][30] = ta.STDDEV(real=df.Close, timeperiod=interval)
+                    tis[i][31] = ta.VAR(real=df.Close, timeperiod=interval)
+
+                images = np.zeros((len(df), IMAGE_SIZE, IMAGE_SIZE))
+
+                for day_idx in range(len(df)):
+                    images[day_idx] = tis[:, :, day_idx]
+            
             images_new = images.copy()
 
             delete_idx = []
@@ -146,7 +161,10 @@ def main():
 
             scaled_images = []
             for idx, im in enumerate(images_new):
-                scaled_im = minmax_scale(im, (0,255)).astype(int)
+                if not ONE_D:
+                    scaled_im = minmax_scale(im, (0,255)).astype(int)
+                else:
+                    scaled_im = im
                 scaled_images.append(scaled_im)
 
             for idx, (scalar, image) in enumerate(zip(scalars, scaled_images),
